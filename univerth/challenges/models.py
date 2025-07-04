e6faaebd36db246a785644ce8621718e7505d54c
ruddmslee <dlruddms326@naver.com>
2025-07-04 19:09:21 +0900

    Feat: 모델 수정

--- univerth/challenges/models.py
+++ univerth/challenges/models.py
@@ -14,11 +14,11 @@ class Challenge(models.Model):
     challenge_name=models.CharField(max_length=50)
     participant_num = models.IntegerField(default=0)
     creator = models.ForeignKey(to=User, on_delete=models.CASCADE, related_name="challenge_creator")
-    start_at = models.DateTimeField()
-    end_at = models.DateTimeField()
+    start_at = models.DateField()
+    end_at = models.DateField()
     description = models.TextField()
     participants = models.ManyToManyField(to=User, related_name="challenge_participants")
-    # 중간 테이블(챌린지 이용자)는 어차피 필드가 챌린지랑 유저밖에 없어서 따로 안만들고 ManyToMany로 넣었는데 문제 있거나 수정하고싶음 말씀 주세요..!
+
     def __str__(self):
         return self.challenge_name
     
@@ -26,20 +26,18 @@ class Feed(models.Model):
     image=models.ImageField(upload_to=upload_filepath, blank=True)
     content=models.TextField()
     challenge=models.ForeignKey(to=Challenge, on_delete=models.CASCADE, related_name="challenge_feeds")
-    author=models.ForeignKey(to=User, on_delete=models.CASCADE, related_name="feeds_author")
-    created_at=models.DateField(auto_now_add=True)
+    writer=models.ForeignKey(to=User, on_delete=models.CASCADE, related_name="feeds_author")
+    created_at=models.DateTimeField(auto_now_add=True)
     like=models.ManyToManyField(to=User, related_name="like_feeds")
 
     def __str__(self):
-        return self.content[:10] 
-    #우선 콘텐트 앞 10글자 리턴하게 했는데 나중에 수정해주셔도 됩니다!
+        return self.content[:10]  #콘텐트 앞 10글자 리턴
 
-#생각해보니 저희가 댓글 모델을 ERD에 안추가해서 따로 추가했습니다..!
 class Comment(models.Model):
     feed=models.ForeignKey(to=Feed, on_delete=models.CASCADE, related_name="comments")
     content=models.TextField()
-    created_at=models.DateField(auto_now_add=True)
-    author=models.ForeignKey(to=User, on_delete=models.CASCADE, related_name="comments_author")
+    created_at=models.DateTimeField(auto_now_add=True)
+    writer=models.ForeignKey(to=User, on_delete=models.CASCADE, related_name="comments_author")
 
     def __str__(self):
         return f'[{self.author.nickname}] - {self.content}'
