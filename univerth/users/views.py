a22f820e34fbaaec5c1183e21925d3f415faafca
ruddmslee <dlruddms326@naver.com>
2025-07-05 00:20:45 +0900

    Feat: 회원가입시 대학이름 지원 구현

--- univerth/users/views.py
+++ univerth/users/views.py
@@ -1,12 +1,27 @@
 from django.shortcuts import render, redirect
 from django.http import JsonResponse
 from .forms import SignupForm
-#from django.views.decorators.csrf import csrf_exempt
-#@csrf_exempt
+from .models import Univ
+from django.views.decorators.csrf import csrf_exempt
 
+@csrf_exempt
 def signup(request):
     if request.method == "POST":
-        form = SignupForm(request.POST)
+        data = request.POST.copy()
+        univ_name = data.get("univ")
+
+        if univ_name and not univ_name.isdigit():
+            try:
+                univ = Univ.objects.get(univ_name = univ_name)
+                data['univ'] = univ.id
+            except Univ.DoesNotExist:
+                return JsonResponse({
+                    "signup_success": False,
+                    "errors": {"univ": ["존재하지 않는 학교입니다."]}
+                })
+
+        form = SignupForm(data)
+
         if form.is_valid():
             form.save()
             return JsonResponse({
@@ -20,4 +35,4 @@ def signup(request):
             })
     else:
         form = SignupForm()
-        return render(request, "signup.html", {form:'form'})
+        return render(request, "signup.html", {'form':form})
