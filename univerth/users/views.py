c64d6841fef1735eb126939f55eee38c50239505
ruddmslee <dlruddms326@naver.com>
2025-07-04 21:50:22 +0900

    Feat: 기본 회원가입 구현 (학교, 이메일 인증 제외)

--- univerth/users/views.py
+++ univerth/users/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import JsonResponse
+from .forms import SignupForm
+#from django.views.decorators.csrf import csrf_exempt
+#@csrf_exempt
 
-# Create your views here.
+def signup(request):
+    if request.method == "POST":
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return JsonResponse({
+                "signup_success": True,
+                "message": "회원가입이 완료되었습니다"
+            })
+        else:
+            return JsonResponse({
+                "signup_success": False,
+                "errors": form.errors
+            })
+    else:
+        form = SignupForm()
+        return render(request, "signup.html", {form:'form'})
