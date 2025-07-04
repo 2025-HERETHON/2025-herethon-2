f63873d049d1b904bacde58f5ba4c1c1c0ae0ce9
ruddmslee <dlruddms326@naver.com>
2025-07-05 00:23:04 +0900

    Feat: User 모델 수정

--- univerth/users/models.py
+++ univerth/users/models.py
@@ -1,17 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-
 class Univ(models.Model):
     univ_name=models.CharField(max_length=20)
     point=models.IntegerField(default=0)
-    rank=models.IntegerField()
+    rank=models.IntegerField(default=417)
 
 class User(AbstractUser):
     email=models.CharField(max_length=30, unique=True, null=False, blank=False)
     nickname=models.CharField(max_length=20, unique=True)
     is_verified=models.BooleanField(default=False)
-    univ=models.ForeignKey(to=Univ, on_delete=models.CASCADE, related_name="students")
+    univ=models.ForeignKey(to=Univ, on_delete=models.CASCADE, related_name="students", null=True, blank=True)
 
-    def __str__(self):
-        return f"{self.nickname} ({self.univ.univ_name})"
+    #def __str__(self):
+    #    return f"{self.nickname} ({self.univ.univ_name})"
