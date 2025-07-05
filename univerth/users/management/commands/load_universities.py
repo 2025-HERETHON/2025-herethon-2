7c4d9ae22cf293aa050925abb2cd02b54092ed6f
ruddmslee <dlruddms326@naver.com>
2025-07-05 23:22:26 +0900

    Feat: 학교 이메일 저장하도록 수정

--- univerth/users/management/commands/load_universities.py
+++ univerth/users/management/commands/load_universities.py
@@ -6,15 +6,15 @@ class Command(BaseCommand):
     help = '엑셀 파일을 읽어 DB에 저장합니다'
 
     def handle(self, *args, **options):
-        wb = openpyxl.load_workbook('대학이름.xlsx')
+        wb = openpyxl.load_workbook('대학 이름 이메일.xlsx')
         ws = wb.active
 
         universities = []
-        for row in ws.iter_rows(min_row=2, min_col=1, max_col=1, values_only=True):
-            name = row[0]
+        for row in ws.iter_rows(min_row=1, min_col=1, max_col=2, values_only=True):
+            name, email_domain = row
             if not name or str(name).strip() == '':
                 continue
-            universities.append(Univ(univ_name=name))
+            universities.append(Univ(univ_name=name, email_domain=email_domain))
 
         Univ.objects.bulk_create(universities)
         self.stdout.write(self.style.SUCCESS(f"{len(universities)}개 대학 저장 완료"))
