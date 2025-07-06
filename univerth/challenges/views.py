0343e7c73cdf8a2e26462b3b32a540f338f8b186
yslee <yeongseo6142@naver.com>
2025-07-07 01:59:23 +0900

    BE 랭킹 조회 구현

--- univerth/challenges/views.py
+++ univerth/challenges/views.py
@@ -3,7 +3,7 @@ from .models import *
 
 #챌린지 세부 내용 보여주기 (챌린지 설명+피드 리스트)
 def challenge_detail(request, id): 
-    challenge=get_object_or_404(Challenge, id=id)
+    challenge=get_object_or_404(Challenge, id=id) 
     feeds = challenge.challenge_feeds.all().order_by('-created_at')
     return render(request, 'challenge_detail.html', {'challenge':challenge, 'feeds':feeds})
 
@@ -60,6 +60,17 @@ def create_feed(request, challenge_id):
             writer=request.user,
             challenge=challenge,
         )
+
+        if request.user.is_authenticated:
+            request.user.user_point += 1
+            request.user.save()
+            
+            if request.user.univ:
+                request.user.univ.univ_point = sum(
+                    student.user_point for student in request.user.univ.students.all()
+                )
+                request.user.univ.save()
+
         return redirect('challenge_detail',id=challenge.id)
     return render(request, 'create_feed.html', {'challenge': challenge})
 
