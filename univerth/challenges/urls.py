53bc1e63aa57c239fcc707ce986de3135106f762
yslee <yeongseo6142@naver.com>
2025-07-07 19:19:09 +0900

    BE 피드 상세 페이지 조회/댓글+좋아요 기능 구현

--- univerth/challenges/urls.py
+++ univerth/challenges/urls.py
@@ -6,15 +6,19 @@ from .views_list import *
 app_name = "challenges"
 
 urlpatterns = [
-    path('challenge-detail/<int:id>/', challenge_detail, name='challenge_detail'),
+    path('challenge-detail/<int:challenge_id>/', challenge_detail, name='challenge_detail'),
     path('join-challenge/<int:challenge_id>/', join_challenge, name='join_challenge'),
     path('exit-challenge/<int:challenge_id>/', exit_challenge, name='exit_challenge'),
     path('create-feed/', create_feed, name='create_feed'),
-    path('update-feed/<int:id>/', update_feed, name='update_feed'),
-    path('delete-feed/<int:id>/', delete_feed, name='delete_feed'),
-    path('feeds/<int:feed_id>/add-like/', add_like, name='add_like'),
-    path('feeds/<int:feed_id>/remove-like/', remove_like, name='remove_like'),
+    path('update-feed/<int:feed_id>/', update_feed, name='update_feed'),
+    path('delete-feed/<int:feed_id>/', delete_feed, name='delete_feed'),
+    path('feeds/<int:feed_id>/toggle-like/', toggle_like, name='toggle_like'),
     path('my/', challenge_list_my, name="challenge_list_my"),
     path('popular/', challenge_list_popular, name="challenge_list_popular"),
     path('new/', create_challenge, name='create_challenge'),
+    path('feeds/<int:feed_id>/create-comment/', create_comment, name='create_comment'),
+    path('feeds/<int:feed_id>/delete-comment/<int:comment_id>/', delete_comment, name='delete_comment'),
+    path('feeds/<int:feed_id>/update-comment/<int:comment_id>/', update_comment, name='update_comment'),
+    path('feeds/<int:feed_id>/data/', feed_data, name='feed_data'),
+    path('feeds/<int:feed_id>/', feed_detail, name='feed_detail'),
 ]
--- univerth/challenges/views.py
+++ univerth/challenges/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import *
+from django.http import JsonResponse
+from django.views.decorators.http import require_POST
 
 #챌린지 세부 내용 보여주기 (챌린지 설명+피드 리스트)
 def challenge_detail(request, id): 
     challenge=get_object_or_404(Challenge, id=id) 
     feeds = challenge.challenge_feeds.all().order_by('-created_at')
-    return render(request, 'challenge_detail.html', {'challenge':challenge, 'feeds':feeds})
+    return render(request, 'challenges/challenge_detail.html', {'challenge':challenge, 'feeds':feeds})
 
 #챌린지 참여 함수
 def join_challenge(request, challenge_id):
@@ -16,7 +18,7 @@ def join_challenge(request, challenge_id):
         challenge.participants.add(user)
         challenge.participant_num+=1
         challenge.save()
-        return redirect('challenge_detail', id=challenge.id)
+        return redirect('challenges:challenge_detail', id=challenge.id)
     
 #챌린지 나가기 함수 
 def exit_challenge(request, challenge_id):
@@ -27,25 +29,24 @@ def exit_challenge(request, challenge_id):
         challenge.participants.remove(user)
         challenge.participant_num-=1
         challenge.save()
-        return redirect('challenge_detail', id=challenge.id)
+        return redirect('challenges:challenge_detail', id=challenge.id)
     
-#좋아요
-def add_like(request, feed_id):
-    feed=get_object_or_404(Feed, id=feed_id)
-    user=request.user
-
-    if user not in feed.like.all():
-        feed.like.add(user) #템플릿에서 개수 불러올 땐 {{feed.like.count}}
-    return redirect('feed_detail', feed_id)
-
-#좋아요 취소
-def remove_like(request, feed_id):
-    feed=get_object_or_404(Feed, id=feed_id)
-    user=request.user
+@require_POST
+def toggle_like(request, feed_id):
+    feed = get_object_or_404(Feed, id=feed_id)
+    user = request.user
 
     if user in feed.like.all():
-        feed.like.remove(user) #템플릿에서 개수 불러올 땐 {{feed.like.count}}
-    return redirect('feed_detail', feed_id)
+        feed.like.remove(user)
+        liked = False
+    else:
+        feed.like.add(user)
+        liked = True
+
+    return JsonResponse({
+        'liked': liked,
+        'like_count': feed.like.count(),
+    })
 
 #피드 작성
 def create_feed(request, challenge_id):
@@ -64,15 +65,15 @@ def create_feed(request, challenge_id):
         if request.user.is_authenticated:
             request.user.user_point += 1
             request.user.save()
-            
+
             if request.user.univ:
                 request.user.univ.univ_point = sum(
                     student.user_point for student in request.user.univ.students.all()
                 )
                 request.user.univ.save()
 
-        return redirect('challenge_detail',id=challenge.id)
-    return render(request, 'create_feed.html', {'challenge': challenge})
+        return redirect('challenges:challenge_detail',id=challenge.id)
+    return render(request, 'challenges/create_feed.html', {'challenge': challenge})
 
 #피드 수정
 def update_feed(request, id):
@@ -87,12 +88,69 @@ def update_feed(request, id):
                 feed.image.delete()
             feed.image = image
         feed.save()
-        return redirect('challenge_detail', id=feed.challenge.id)
-    return render(request, 'update.html', {'feed':feed})
+        return redirect('challenges:challenge_detail', id=feed.challenge.id)
+    return render(request, 'challenges/update.html', {'feed':feed})
 
 #피드 삭제 
 def delete_feed(request, id):
     feed=get_object_or_404(Feed, id=id)
     feed.delete()
-    return redirect('challenge_detail', id=feed.challenge.id)
+    return redirect('challenges:challenge_detail', id=feed.challenge.id)
 
+#댓글 생성
+def create_comment(request, feed_id):
+    feed=get_object_or_404(Feed, id=feed_id)
+    if request.method=="POST":
+        content=request.POST.get('content')
+
+        Comment.objects.create(
+            feed=feed,
+            content=content,
+            writer=request.user
+        )
+        return redirect('challenges:feed_detail', feed_id)
+    return redirect('challenges:challenge_detail') 
+    # 댓글 수는 {{feed.comments.count}} 로 가져오기
+
+#댓글 수정
+def update_comment(request, id):
+    comment = get_object_or_404(Comment, id=id)
+
+    if request.method == "POST":
+        content = request.POST.get('content')
+        if content:
+            comment.content = content
+            comment.save()
+        return redirect('challenges:feed_detail', comment.feed.id)
+
+#댓글 삭제
+def delete_comment(request, id):
+    comment = get_object_or_404(Comment, id=id)
+    comment.delete()
+    return redirect('challenges:feed_detail', comment.feed.id)
+
+#피드 상세 조회. JsonResponse로 데이터만 보냄
+def feed_data(request, feed_id):
+    feed = get_object_or_404(Feed, id=feed_id)
+
+    data = {
+        'writer': feed.writer.nickname,
+        'content': feed.content,
+        'created_at': feed.created_at.strftime('%m/%d %H:%M'),  
+        'like_count': feed.like.count(),
+        'comment_count': feed.comments.count(),
+        'comments': [
+            {
+                'writer': comment.writer.nickname,
+                'content': comment.content,
+                'created_at': comment.created_at.strftime('%m/%d %H:%M')
+            }
+            for comment in feed.comments.all().order_by('created_at')
+        ]
+    }
+
+    return JsonResponse(data)
+
+# 피드 상세 조회 페이지 html 렌더링
+def feed_detail(request, feed_id):
+    return render(request, 'challenges/feed_detail.html', {'feed_id': feed_id})
