cddbb22660caad567f24bad3fc8c951bbe9eebf1
ruddmslee <dlruddms326@naver.com>
2025-07-07 16:40:17 +0900

    Feat: 홈에서 날짜별 퀴즈 상세 조회

--- univerth/users/views_home.py
+++ univerth/users/views_home.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from .models import *
+from quiz.models import *
+from datetime import date
 
 # 상위 세개 대학 랭킹
 def univ_rankings():
@@ -28,12 +30,49 @@ def my_ranking(user):
         "ranking": my_rank
     }
 
-# 상위 랭킹 + 내 랭킹 JsonResponse로 넘겨주는 함수
-def ranks(request):
+def home(request):
     ranking = univ_rankings()
     my_rank = my_ranking(request.user)
 
-    return JsonResponse({
+    return render(request, "home.html", {
         'ranking': ranking,
         'my_ranking': my_rank
+    })
+
+def home_quiz(request):
+    year = request.GET.get('year')
+    month = request.GET.get('month')
+    day = request.GET.get('day')
+
+    try:
+        quiz_date = date(int(year), int(month), int(day))
+    except:
+        return JsonResponse({'error': '유효한 날짜를 입력하세요'})
+    
+    quiz = Quiz.objects.filter(date=quiz_date).first()
+
+    if not quiz:
+        return JsonResponse({'error':'해당 날짜의 퀴즈가 없습니다.'})
+    
+    weekday = quiz_date.weekday()
+    if weekday == 0:
+        type = '소비 습관'
+    elif weekday == 1:
+        type = '식생활'
+    elif weekday == 2:
+        type = '이동 수단'
+    elif weekday == 3:
+        type = '캠퍼스 생활'
+    elif weekday == 4:
+        type = '디지털 습관'
+    elif weekday == 5:
+        type = '재사용/재활용'
+    elif weekday == 6:
+        type = '사회적 인식 및 트렌드'
+
+    return JsonResponse({
+        'type': type,
+        'question' : quiz.question,
+        'answer': quiz.answer,
+        'mission' : quiz.mission
     })
