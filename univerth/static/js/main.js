da61f7fa9624651d540bba6521c8e76df3109c75
Zero020 <chi020524@naver.com>
2025-07-09 01:17:27 +0900

    [Fix] 홈/퀴즈 페이지 데이터 연동

--- univerth/static/js/main.js
+++ univerth/static/js/main.js
@@ -1,36 +1,30 @@
 /*----공통------*/
 
 //상단/하단 nav-bar
-export async function loadNavbar(containerSelector = ".home-container") {
-  try {
-    const res = await fetch("../templates/navbar.html");
-    const html = await res.text();
-    document.querySelector(containerSelector).insertAdjacentHTML("afterbegin", html);
+export function loadNavbar(containerSelector = ".home-container") {
 
-    // 모든 탭 아이콘을 기본 이미지로 되돌리기
-    document.querySelector("#nav-home img").src = "../static/images/navbar/home_basic.png";
-    document.querySelector("#nav-quiz img").src = "../static/images/navbar/quiz_basic.png";
-    document.querySelector("#nav-map img").src = "../static/images/navbar/map_basic.png";
-    document.querySelector("#nav-challenge img").src = "../static/images/navbar/challenge_basic.png";
+  // 모든 탭 아이콘을 기본 이미지로 되돌리기
+  document.querySelector("#nav-home img").src = "../static/images/navbar/home_basic.png";
+  document.querySelector("#nav-quiz img").src = "../static/images/navbar/quiz_basic.png";
+  document.querySelector("#nav-map img").src = "../static/images/navbar/map_basic.png";
+  document.querySelector("#nav-challenge img").src = "../static/images/navbar/challenge_basic.png";
 
-    switch (containerSelector) {
-      case ".home-container":
-        document.querySelector("#nav-home img").src = "../static/images/navbar/home_select.png";
-        break;
-      case ".quiz-container":
-        document.querySelector("#nav-quiz img").src = "../static/images/navbar/quiz_select.png";
-        break;
-      /*case ".map-container":
-        document.querySelector("#nav-map img").src = "../static/images/navbar/map_select.png";
-        break;
-      case ".challenge-container":
-        document.querySelector("#nav-challenge img").src = "../static/images/navbar/challenge_select.png";
-        break;*/
-    }
-
-  } catch (err) {
-    console.error("Navbar 로드 실패:", err);
+  switch (containerSelector) {
+    case ".home-container":
+      document.querySelector("#nav-home img").src = "../static/images/navbar/home_select.png";
+      break;
+    case ".quiz-container":
+      document.querySelector("#nav-quiz img").src = "../static/images/navbar/quiz_select.png";
+      break;
+    /*case ".map-container":
+      document.querySelector("#nav-map img").src = "../static/images/navbar/map_select.png";
+      break;
+    case ".challenge-container":
+      document.querySelector("#nav-challenge img").src = "../static/images/navbar/challenge_select.png";
+      break;*/
   }
+
+
 }
 
 
