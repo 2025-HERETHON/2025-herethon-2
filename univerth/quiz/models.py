b9093bd6878530b836847565819348ec25ff0ebc
yslee <yeongseo6142@naver.com>
2025-07-09 00:51:23 +0900

    BE 카테고리 필드 제거

--- univerth/quiz/models.py
+++ univerth/quiz/models.py
@@ -1,11 +1,6 @@
 from django.db import models
 from users.models import *
 
-class Category(models.Model):
-    name=models.CharField(max_length=20)
-
-    def __str__(self):
-        return self.name
 
 class Quiz(models.Model):
     date = models.DateField()
