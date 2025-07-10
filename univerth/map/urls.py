09395e10bf40e59fcab40a6529b6630e686eb6fb
ruddmslee <dlruddms326@naver.com>
2025-07-10 14:21:48 +0900

    Feat: 모든 가게 정보 조회 구현, url 추가

--- univerth/map/urls.py
+++ univerth/map/urls.py
@@ -6,5 +6,7 @@ app_name = 'map'
 
 urlpatterns = [
     path('add/', new_store, name='new_store'),
-    
+    path('', map_main, name="map_main"),
+    path('store/<int:id>/', show_store, name="show_store"),
+    path('store/list/', get_stores, name='get_stores'),
 ]
